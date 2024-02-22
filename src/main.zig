4ad09b2bb6bddb64526b35d2977a94bb28e4e194
Mauricio Rojas <mrojasb2000@gmail.com>
2024-02-22 14:19:09 -0300

    feat: adding basic web server

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,11 @@
 const std = @import("std");
-
+const net = std.net;
+const StreamServer = net.StreamServer;
+const Address = net.Address;
+const print = std.debug.print;
 pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var stream_server = StreamServer.init(.{});
+    const address = try Address.resolveIp("127.0.0.1", 8080);
+    try stream_server.listen(address);
+    print("Server is running.\n", .{});
 }
