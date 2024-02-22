3d0a973c87b7e97cdedf838e3576ca099e717ac2
Mauricio Rojas <mrojasb2000@gmail.com>
2024-02-22 14:45:28 -0300

    feat: adding basic web server listering port 8080

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,14 @@ const Address = net.Address;
 const print = std.debug.print;
 pub fn main() !void {
     var stream_server = StreamServer.init(.{});
+    defer stream_server.close();
     const address = try Address.resolveIp("127.0.0.1", 8080);
     try stream_server.listen(address);
     print("Server is running.\n", .{});
+
+    while (true) {
+        const connection = try stream_server.accept();
+        try connection.stream.writer().print("Hello World from Zig Lang", .{});
+        connection.stream.close();
+    }
 }
