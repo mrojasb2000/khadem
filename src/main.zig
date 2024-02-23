2fb13c141b69dc479919cd4fa16e13069dce96a0
Mauricio Rojas <mrojasb2000@gmail.com>
2024-02-23 13:36:18 -0300

    refactor: adding HTTP context

--- src/main.zig
+++ src/main.zig
@@ -15,7 +15,7 @@ pub fn main() !void {
     defer stream_server.close();
     const address = try Address.resolveIp("127.0.0.1", 8080);
     try stream_server.listen(address);
-    print("Server is running.\n", .{});
+    print("Server is running...\n", .{});
 
     while (true) {
         const connection = try stream_server.accept();
@@ -23,34 +23,99 @@ pub fn main() !void {
     }
 }
 
+const ParsingError = error{
+    MethodNotValid,
+    VersionNotValid,
+};
+
+const Method = enum {
+    GET,
+    POST,
+    PUT,
+    PATCH,
+    OPTION,
+    DELETE,
+
+    pub fn fromString(s: []const u8) !Method {
+        if (std.mem.eql(u8, "GET", s)) return .GET;
+        if (std.mem.eql(u8, "POST", s)) return .POST;
+        if (std.mem.eql(u8, "PUT", s)) return .PUT;
+        if (std.mem.eql(u8, "PATCH", s)) return .PATCH;
+        if (std.mem.eql(u8, "OPTION", s)) return .OPTION;
+        if (std.mem.eql(u8, "DELETE", s)) return .DELETE;
+        return ParsingError.MethodNotValid;
+    }
+};
+
+const Version = enum {
+    @"1.1",
+    @"2",
+
+    pub fn fromString(s: []const u8) !Version {
+        if (std.mem.eql(u8, "HTTP/1.1", s)) return .@"1.1";
+        if (std.mem.eql(u8, "HTTP/2", s)) return .@"2";
+        return ParsingError.VersionNotValid;
+    }
+};
+
+const HTTPContext = struct {
+    method: Method,
+    uri: []const u8,
+    version: Version,
+    headers: std.StringHashMap([]const u8),
+    stream: net.Stream,
+
+    pub fn body(self: *HTTPContext) net.Stream.Reader {
+        return self.stream.reader();
+    }
+
+    pub fn response(self: *HTTPContext) net.Stream.Writer {
+        return self.stream.writer();
+    }
+
+    pub fn printRequestDebug(self: *HTTPContext) void {
+        print("Method: {any}\nUri: {s}\nVersion: {any}\n", .{ self.method, self.uri, self.version });
+        var headers_iter = self.headers.iterator();
+        while (headers_iter.next()) |entry| {
+            print("{s}: {s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+        }
+    }
+
+    pub fn init(allocator: std.mem.Allocator, stream: net.Stream) !HTTPContext {
+        var first_line = try stream.reader().readUntilDelimiterAlloc(allocator, '\n', std.math.maxInt(usize));
+        first_line = first_line[0 .. first_line.len - 1];
+        var first_line_iter = std.mem.split(u8, first_line, " ");
+
+        const method = first_line_iter.next().?;
+        const uri = first_line_iter.next().?;
+        const version = first_line_iter.next().?;
+
+        var headers = std.StringHashMap([]const u8).init(allocator);
+
+        while (true) {
+            var line = try stream.reader().readUntilDelimiterAlloc(allocator, '\n', std.math.maxInt(usize));
+            if (line.len == 1 and std.mem.eql(u8, line, "\r")) break;
+
+            line = line[0..line.len];
+            var line_iter = std.mem.split(u8, line, ":");
+            const key = line_iter.next().?;
+            var value = line_iter.next().?;
+            if (value[0] == ' ') value = value[1..];
+            try headers.put(key, value);
+        }
+
+        return HTTPContext{
+            .headers = headers,
+            .method = try Method.fromString(method),
+            .version = try Version.fromString(version),
+            .uri = uri,
+            .stream = stream,
+        };
+    }
+};
+
 fn handler(allocator: std.mem.Allocator, stream: net.Stream) !void {
     defer stream.close();
-
-    var first_line = try stream.reader().readUntilDelimiterAlloc(allocator, '\n', std.math.maxInt(usize));
-    first_line = first_line[0..first_line.len];
-    var first_line_iter = std.mem.split(u8, first_line, " ");
-
-    const method = first_line_iter.next().?;
-    const url = first_line_iter.next().?;
-    const version = first_line_iter.next().?;
-
-    var headers = std.StringHashMap([]const u8).init(allocator);
-
-    while (true) {
-        var line = try stream.reader().readUntilDelimiterAlloc(allocator, '\n', std.math.maxInt(usize));
-        if (line.len == 1 and std.mem.eql(u8, line, "\r")) break;
-
-        line = line[0..line.len];
-        var line_iter = std.mem.split(u8, line, ":");
-        const key = line_iter.next().?;
-        var value = line_iter.next().?;
-        if (value[0] == ' ') value = value[1..];
-        try headers.put(key, value);
-    }
-
-    print("Method: {s}\nUrl: {s}\nVersion: {s}\n", .{ method, url, version });
-    var headers_iter = headers.iterator();
-    while (headers_iter.next()) |entry| {
-        print("{s}: {s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
-    }
+    var http_context = try HTTPContext.init(allocator, stream);
+    http_context.printRequestDebug();
 }
